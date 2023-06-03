fe68778e76c1e82c1e1ad941c180bb9387a25dba
rekan <rekan4shwan2002@gmail.com>
2023-06-03 22:48:35 +0300

    some

--- js/js.js
+++ js/js.js
@@ -1,17 +1,6 @@
 function downloadPDF() {
-    // Replace 'path/to/your/file.pdf' with the actual path to your PDF file
-    var fileUrl = 'path/to/your/file.pdf';
-  
-    // Create an anchor element
-    var a = document.createElement('a');
-    
-    // Set the href attribute of the anchor element to the PDF file URL
-    a.href = fileUrl;
-    
-    // Set the 'download' attribute to specify the filename
-    a.download = '/pdf/REKAN.pfd';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+    let link = document.createElement('a');
+    link.href = '/pdf/REKAN.pdf';
+    link.download = 'REKAN.pdf';
+    link.click();
   }
-  document.getElementById('resume_link').addEventListener('click', downloadPDF);
