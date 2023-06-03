5d7d1d7473d58260c0bb8b7b1cabea3623e19be6
rekan <rekan4shwan2002@gmail.com>
2023-06-04 00:22:40 +0300

    some

--- js/js.js
+++ js/js.js
@@ -4,3 +4,18 @@ function downloadPDF() {
     link.download = 'REKAN.pdf';
     link.click();
   }
+function dfunction() {
+  document.getElementById("languages").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
