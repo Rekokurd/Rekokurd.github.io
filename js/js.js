f475ff5a7cbc83ef384f20c27888533678ecde77
rekan <rekan4shwan2002@gmail.com>
2023-06-04 13:27:53 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -4,6 +4,23 @@ function downloadPDF() {
     link.download = 'REKAN.pdf';
     link.click();
   }
+  function downloadPDF() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    if (/android/i.test(userAgent)) {
+      // For Android devices, open the PDF in a new tab
+      window.open('/pdf/REKAN.pdf', '_blank');
+    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      // For iOS devices, open the PDF in a new window
+      window.open('/pdf/REKAN.pdf');
+    } else {
+      // For other devices, initiate the direct download
+      var link = document.createElement('a');
+      link.href = '/pdf/REKAN.pdf';
+      link.download = 'REKAN.pdf';
+      link.click();
+    }
+  }
 function dfunction() {
   document.getElementById("languages").classList.toggle("show");
 }
