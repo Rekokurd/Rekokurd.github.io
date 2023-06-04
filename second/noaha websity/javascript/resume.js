b73d7ee6aa0fad3655ac3db59c53726220e2b276
rekan <rekan4shwan2002@gmail.com>
2023-06-05 00:25:32 +0300

    some

--- second/noaha websity/javascript/resume.js
+++ second/noaha websity/javascript/resume.js
@@ -21,25 +21,38 @@ function downloadPDF() {
     link.click();
   }
 }
-function dfunction() {
-document.getElementById("languages").classList.toggle("show");
-}
-window.onclick = function(event) {
-if (!event.target.matches('.dropbtn')) {
-  var dropdowns = document.getElementsByClassName("dropdown-content");
-  var i;
-  for (i = 0; i < dropdowns.length; i++) {
-    var openDropdown = dropdowns[i];
-    if (openDropdown.classList.contains('show')) {
-      openDropdown.classList.remove('show');
-    }
-  }
-}
-}
-const input = document.getElementById('myInput');
+
+const input = document.getElementsByClassName('input');
 input.addEventListener('focus', function() {
-this.removeAttribute('placeholder');
+  this.removeAttribute('placeholder');
 });
 input.addEventListener('blur', function() {
-this.setAttribute('placeholder', 'Enter your text');
+  this.setAttribute('placeholder', 'Enter your text');
 });
+
+// newwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+emailjs.init("IKYmqgdPB80BecU28");
+
+function sendEmail() {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Get the input values
+    var name = document.getElementById('nameinput').value;
+    var email = document.getElementById('emailinput').value;
+    var message = document.getElementById('messageinput').value;
+
+    // Send the email using EmailJS
+    emailjs.send("service_wkg7o9m", "template_f74zkcy", {
+        to_email: email,
+        from_name: name,
+        message: message
+    })
+    .then(function(response) {
+        console.log('Email sent successfully', response);
+        // You can add a success message or any other desired action here
+    }, function(error) {
+        console.error('Email sending failed', error);
+        // You can add an error message or any other desired action here
+    });
+}
+
