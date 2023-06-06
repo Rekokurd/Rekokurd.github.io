951f868373e2b2c18940bdc9fb849f78fa8c28b3
rekan <rekan4shwan2002@gmail.com>
2023-06-07 00:03:57 +0300

    some changes

--- javascript/resume.js
+++ javascript/resume.js
@@ -5,17 +5,17 @@ function downloadPDF() {
   link.click();
 }
 function downloadPDF() {
-  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  let userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
   if (/android/i.test(userAgent)) {
-    // For Android devices, open the PDF in a new tab
+   
     window.open('/pdf/REKAN.pdf', '_blank');
   } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    // For iOS devices, open the PDF in a new window
+    
     window.open('/pdf/REKAN.pdf');
   } else {
-    // For other devices, initiate the direct download
-    var link = document.createElement('a');
+    
+    let link = document.createElement('a');
     link.href = '/pdf/REKAN.pdf';
     link.download = 'REKAN.pdf';
     link.click();
@@ -23,25 +23,23 @@ function downloadPDF() {
 }
 
 (function() {
-  emailjs.init("IKYmqgdPB80BecU28"); // Replace with your EmailJS user ID
+  emailjs.init("IKYmqgdPB80BecU28"); // EmailJS user ID
 
   document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Get the form data
-    var formData = {
+    let formData = {
       name: document.getElementById('name').value,
       email: document.getElementById('email').value,
       message: document.getElementById('message').value
     };
 
-    // Send the form data via EmailJS
     emailjs.send("service_wkg7o9m", "template_f74zkcy", formData)
       .then(function(response) {
         document.getElementById('response').innerHTML = "your message snet!";
         console.log('SUCCESS!', response.status, response.text);
       }, function(error) {
-        document.getElementById('response').innerHTML = "Error occurred while submitting the form.";
+        document.getElementById('response').innerHTML = "Error occurred while submitting the message.";
         console.log('FAILED...', error);
       });
   });
